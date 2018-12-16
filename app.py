e200e6ca6c1e582c7c7f5e352b7148db86cdd830
Ahmed Hammad <hammad97official@gmail.com>
2018-12-16 03:27:03 +0200

    Loaded labels + matriciezed them

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os, numpy, random, struct
 
-# Load Images
+# Load images
 trainingInput = open(os.path.relpath("datasets/train-images-idx3-ubyte"), 'rb')
 trainingInput.seek(0)
 MSB = struct.unpack('>4B', trainingInput.read(4))
@@ -16,6 +16,18 @@ images = numpy.reshape(images, (imageCount, rows, columns))
 
 # Faltten to vector
 vectors = numpy.reshape(images, (imageCount, rows*columns)) / 255
+
+
+# Load labels
+labelInput = os.path.relpath("datasets/train-labels-idx3-ubyte"), 'rb')
+labelInput.seek(0)
+MSB = struct.unpack('>4B', labelInput.read(4))
+labelCount = struct.unpack('>I', labelInput.read(4))[0]
+
+
+# Convert labels to matrices
+labels = numpy.asarray(struct.unpack('>' + 'B'*labelCount, labelInput.read(labelCount))).reshape((labelCount))
+
 
 # Plot
 # Start training
