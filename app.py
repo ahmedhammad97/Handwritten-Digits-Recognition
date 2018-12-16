ec668e4b920766c88edbdda51cf306102bdc63de
Ahmed Hammad <hammad97official@gmail.com>
2018-12-16 03:19:59 +0200

    Flattened matrices to vectors

--- app.py
+++ app.py
@@ -8,16 +8,14 @@ imageCount = struct.unpack('>I', trainingInput.read(4))[0]
 rows = struct.unpack('>I', trainingInput.read(4))[0]
 columns = struct.unpack('>I', trainingInput.read(4))[0]
 
-print(MSB, imageCount, rows, columns)
-
 
 # Convert to numeric matrix
 images = numpy.asarray(struct.unpack('>' + 'B'*imageCount*rows*columns, trainingInput.read(imageCount*rows*columns)))
 images = numpy.reshape(images, (imageCount, rows, columns))
 
-print(len(images))
 
 # Faltten to vector
-# Add to numpy Array
+vectors = numpy.reshape(images, (imageCount, rows*columns)) / 255
+
 # Plot
 # Start training
